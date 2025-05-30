69fb6e40f07b2bb760333ffe708ca8ae9a465068
Noa <nguyenduynoa198@gmail.com>
2025-05-30 17:10:34 +0700

    add git ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
-    historyApiFallback: true,
+    middlewareMode: true,
   },
   build: {
     rollupOptions: {
